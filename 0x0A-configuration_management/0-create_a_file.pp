587b2e1ffdd3df3d592eb8a29af204497e45c2dc
Ojieide Egbomondion <ojieide@yahoo.com>
2023-06-23 17:04:23 +0000

    Create a file

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,7 +1,8 @@
 file { '/tmp/school':
   ensure  => present,
+  content => 'I love Puppet',
+  mode    => '0744',
   owner   => 'www-data',
   group   => 'www-data',
-  mode    => '0744',
-  content => 'I love Puppet',
 }
+
