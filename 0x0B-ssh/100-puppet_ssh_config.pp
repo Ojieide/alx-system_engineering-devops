118f9235f3ad2b5a9d59dcc2e1f0bfb8b37f0c2e
Ojieide Egbomondion <ojieide@yahoo.com>
2023-06-25 19:46:39 +0000

    Client configuration file (w/ Puppet)

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,10 @@
+# Client SSH configuration file setup using puppet
+file_line { 'Identity file':
+  ensure => 'present',
+  path   => '/etc/ssh/ssh_config',
+  line   => '    IdentityFile ~/.ssh/school',
+}
+file_line { 'disable password login':
+    path    => '/etc/ssh/ssh_config',
+    line    => '    PasswordAuthentication no',
+}
