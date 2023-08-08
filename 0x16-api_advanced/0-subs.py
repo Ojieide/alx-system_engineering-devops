f2c705b1a91e003b203fa98760de476cfcb3c1df
Ojieide Egbomondion <ojieide@yahoo.com>
2023-08-08 14:29:38 +0000

    API advanced

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -7,13 +7,13 @@ import requests
 
 
 def number_of_subscribers(subreddit):
-	""" Returns the number of subscribers for a given subreddit """
-    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
-    response = requests.get(url, allow_redirects=False, headers={
-                            "User-Agent": "Nico@alx/1.0"})
-    if response.status_code != 200:
+    """ Returns the number of subscribers for a given subreddit """
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {
+        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
+    }
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code == 404:
         return 0
-    else:
-        data = response.json()
-        subscribers = data['data']['subscribers']
-        return subscribers
+    results = response.json().get("data")
+    return results.get("subscribers")
